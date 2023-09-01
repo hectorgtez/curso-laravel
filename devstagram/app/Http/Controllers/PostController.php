49d66e013f4ebcb6fb725aeb34de0f08ce94c4a4
hectorgtez <hectorgtez@gmail.com>
2023-09-01 14:39:26 -0700

    [ADD] DevStagram - Adjuntar imagenes DropZone, y creacion y almacenamiento de posts

--- devstagram/app/Http/Controllers/PostController.php
+++ devstagram/app/Http/Controllers/PostController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
@@ -18,5 +20,31 @@ class PostController extends Controller
 
     public function create() {
         return view('posts.create');
+    }
+
+    public function store(Request $request) {
+        // Validacion de datos ingresados
+        $this->validate($request, [
+            "titulo" => "required|max:255",
+            "descripcion" => "required",
+            "imagen" => "required"
+        ]);
+
+        // Creación y almacenamiento de nuevo registro
+        Post::create([
+            "titulo" => $request->titulo,
+            "descripcion" => $request->descripcion,
+            "imagen" => $request->imagen,
+            "user_id" => auth()->user()->username
+        ]);
+
+        // Otra forma de crear el registro
+        // $post = new Post();
+        // $post->titulo = $request->titulo;
+        // $post->descripcion = $request->descripcion;
+        // $post->imagen = $request->imagen;
+        // $post->user_id = auth()->user()->username;
+
+        return redirect()->route("posts.index", auth()->user()->username);
     }
 }
--- devstagram/resources/views/posts/create.blade.php
+++ devstagram/resources/views/posts/create.blade.php
@@ -4,13 +4,19 @@
     Crear nueva publicación
 @endsection
 
+@push('styles')
+    <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
+@endpush
+
 @section('contenido')
     <div class="md:flex md:items-center">
         <div class="md:w-1/2 px-10">
-            Imagen aquí
+            <form action="{{route('imagenes.store')}}" method="POST" enctype="multipart/form-data" id="dropzone" class="dropzone border-dashed border-2 w-full h-96 rounded flex flex-col justify-center items-center">
+                @csrf
+            </form>
         </div>
         <div class="md:w-1/2 p-10 bg-white rounded-lg shaodw-xl mt-10 md:mt-0">
-            <form action="" method="" novalidate>
+            <form action="{{route('posts.store')}}" method="POST" novalidate>
                 @csrf
                 <div class="mb-5">
                     <label for="titulo" class="mb-2 block uppercase text-gray-500 font-bold">Título</label>
@@ -21,8 +27,14 @@
                 </div>
                 <div class="mb-5">
                     <label for="descripcion" class="mb-2 block uppercase text-gray-500 font-bold">Descripción</label>
-                    <textarea id="descripcion" name="descripcion" placeholder="Descripción" class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror">{{old('titulo')}}</textarea>
-                    @error('titulo')
+                    <textarea id="descripcion" name="descripcion" placeholder="Descripción" class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror">{{old('descripcion')}}</textarea>
+                    @error('descripcion')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{$message}}</p>
+                    @enderror
+                </div>
+                <div class="mb-5">
+                    <input name="imagen" type="hidden" value="{{old('imagen')}}">
+                    @error('imagen')
                         <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{$message}}</p>
                     @enderror
                 </div>
