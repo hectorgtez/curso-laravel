81a4b81968ebb9cb0b10c0c8f8956d0751cbf33e
hectorgtez <hectorgtez@gmail.com>
2023-09-04 19:43:13 -0700

    [ADD] DevStagram - Editar username e imagen de perfil, mostrar cantidad de publicaciones en perfil

--- devstagram/resources/views/dashboard.blade.php
+++ devstagram/resources/views/dashboard.blade.php
@@ -8,13 +8,24 @@
     <div class="flex justify-center">
         <div class="w-full md:w-8/12 lg:w-6/12 flex flex-col items-center md:flex-row">
             <div class="w-8/12 lg:w-6/12 px-5">
-                <img src="{{asset('img/usuario.png')}}" alt="Imagen de usuario">
+                <img src="{{$user->imagen ? asset('perfiles').'/'.$user->imagen : asset('img/usuario.png')}}" alt="Imagen de usuario">
             </div>
             <div class="md:w-8/12 lg:w-6/12 px-5 flex flex-col items-center py-10 md:justify-center md:items-start md:py:10">
-                <p class="text-gray-700 text-2xl">{{$user->username}}</p>
+                <div class="flex items-center gap-3">
+                    <p class="text-gray-700 text-2xl">{{$user->username}}</p>
+                    @auth
+                        @if ($user->id === auth()->user()->id)
+                            <a href="{{route('perfil.index')}}" class="text-gray-500 hover:text-gray-600 cursor-pointer h-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-4">
+                                    <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z" />
+                                </svg>
+                            </a>
+                        @endif
+                    @endauth
+                </div>
                 <p class="text-gray-800 text-sm mb-3 font-bold mt-5">0<span class="font-normal"> Seguidores</span></p>
                 <p class="text-gray-800 text-sm mb-3 font-bold">0<span class="font-normal"> Siguiendo</span></p>
-                <p class="text-gray-800 text-sm mb-3 font-bold">0<span class="font-normal"> Posts</span></p>
+                <p class="text-gray-800 text-sm mb-3 font-bold">{{$user->posts->count()}}<span class="font-normal"> Posts</span></p>
             </div>
         </div>
     </div>
