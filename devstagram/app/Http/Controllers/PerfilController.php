03a57ac7e72058b0123f4279d659916d4447a3cf
hectorgtez <hectorgtez@gmail.com>
2023-09-06 16:58:27 -0700

    [ADD] DevStagram - Aplicacion terminada

--- devstagram/app/Http/Controllers/PerfilController.php
+++ devstagram/app/Http/Controllers/PerfilController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
 use Intervention\Image\Facades\Image;
 
@@ -21,6 +22,7 @@ class PerfilController extends Controller
         // Modificar el request
         $request->request->add(["username" => Str::slug($request->username)]);
 
+        // Validar campos del formulario
         $this->validate($request, [
             "username" => ["required", "unique:users,username,".auth()->user()->id, "min:3", "max:20", "not_in:editar-perfil"]
         ]);
@@ -36,11 +38,22 @@ class PerfilController extends Controller
             $imagenServidor->save($imagenPath);
         }
 
-        // Guardar cambios
+        // Crear el objeto del usuario modificado
         $usuario = User::find(auth()->user()->id);
+        $auxImagen = $usuario->imagen; // Variable auxiliar para borrar imagen anterior
         $usuario->username = $request->username;
         $usuario->imagen = $nombreImagen ?? auth()->user()->imagen ?? "";
+
+        // Guardar cambios
         $usuario->save();
+
+        // Borrar imagen de perfil anterior
+        if($request->imagen) {
+            $imagen_path = public_path("perfiles"."/".$auxImagen);
+            if(File::exists($imagen_path)) {
+                unlink($imagen_path);
+            }
+        }
 
         // Redireccionar
         return redirect()->route("posts.index", $usuario->username);
