6306c92792deed995a4c1e55e521f6e2b5d6ad29
hectorgtez <hectorgtez@gmail.com>
2023-09-05 18:48:24 -0700

    [ADD] DevStagram - Componente ListarPost

--- devstagram/resources/views/dashboard.blade.php
+++ devstagram/resources/views/dashboard.blade.php
@@ -46,24 +46,8 @@
         </div>
     </div>
 
-    <section class="container mx-auto mt-10 w-4/5">
+    <section class="container mx-auto mt-10">
         <h2 class="text-4xl text-center font-black my-10">Publicaciones</h2>
-
-        @if($posts->count())
-            <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                @foreach($posts as $post)
-                    <div>
-                        <a href="{{route("posts.show", ['post' => $post, 'user' => $user])}}">
-                            <img src="{{asset("uploads")."/".$post->imagen}}" alt="Imagen de publicación {{$post->titulo}}">
-                        </a>
-                    </div>
-                @endforeach
-            </div>
-            <div class="my-10">
-                {{$posts->links()}}
-            </div>
-        @else
-            <p class="text-gray-600 uppercase text-sm text-center font-bold">No hay publicaciones</p>
-        @endif
+        <x-listar-post :posts="$posts"></x-listar-post>
     </section>
 @endsection
