59a8acb88c42456ed4d4037bd1e27b95b578204a
hectorgtez <hectorgtez@gmail.com>
2023-09-14 18:07:21 -0700

    [ADD] DevJobs - Crear vacantes

--- devjobs/resources/views/vacantes/index.blade.php
+++ devjobs/resources/views/vacantes/index.blade.php
@@ -7,6 +7,11 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @if (session()->has("mensaje"))
+                <div class="border border-green-600 bg-green-100 text-green-600 rounded-md font-bold px-2 py-1 my-3 text-sm">
+                    {{ session("mensaje") }}
+                </div>
+            @endif
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     Mis vacantes...
