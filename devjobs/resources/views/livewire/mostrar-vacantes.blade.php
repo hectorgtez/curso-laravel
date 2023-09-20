e137eeac3b93c0558f3f82234aa285acb50a0de5
hectorgtez <hectorgtez@gmail.com>
2023-09-19 18:48:10 -0700

    [ADD] DevJobs - Editar vacantes, policy para editar vacantes

--- devjobs/resources/views/livewire/mostrar-vacantes.blade.php
+++ devjobs/resources/views/livewire/mostrar-vacantes.blade.php
@@ -15,7 +15,7 @@
                         Candidatos
                     </a>
                     <a
-                        href="#"
+                        href="{{ route('vacantes.edit', $vacante->id) }}"
                         class="text-center items-center px-4 py-2 bg-blue-800 dark:bg-blue-800 border border-transparent rounded-md font-semibold text-xs text-white dark:text-white uppercase tracking-widest hover:bg-blue-700 dark:hover:bg-blue-700 focus:bg-blue-700 dark:focus:bg-blue-700 active:bg-blue-900 dark:active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
                     >
                         Editar
