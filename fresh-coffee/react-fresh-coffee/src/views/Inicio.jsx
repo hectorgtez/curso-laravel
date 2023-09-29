84ab1fe3f6ec5886f186093efe05fc462f37c180
hectorgtez <hectorgtez@gmail.com>
2023-09-28 18:45:00 -0700

    [ADD] FreshCoffee - Implementacion de SWR para consumir API para productos

--- fresh-coffee/react-fresh-coffee/src/views/Inicio.jsx
+++ fresh-coffee/react-fresh-coffee/src/views/Inicio.jsx
@@ -1,10 +1,22 @@
+import useSWR from 'swr'
+
 import Producto from "../components/Producto"
-import { productos as data } from "../data/productos"
 import useQuiosco from "../hooks/useQuiosco"
+import clienteAxios from '../config/axios'
 
 export default function Inicio() {
   const { categoriaActual } = useQuiosco()
-  const productos = data.filter(producto => producto.categoria_id === categoriaActual.id);
+
+  // Consulta SWR
+  const fetcher = () => clienteAxios('/api/productos').then(data => data.data);
+  const { data, error, isLoading } = useSWR('/api/productos', fetcher, {
+    refreshInterval: 1000
+  });
+
+  if (error) return console.log(error);
+  if (isLoading) return 'Cargando...';
+
+  const productos = data.data.filter(producto => producto.categoria_id === categoriaActual.id);
 
   return (
     <>
