eb537d607017b79e86bb085bb07ead30d5b26076
hectorgtez <hectorgtez@gmail.com>
2023-09-28 17:28:54 -0700

    [ADD] FreshCoffee - Implementando Axios y obtener categorias desde API

--- fresh-coffee/react-fresh-coffee/src/context/QuioscoProvider.jsx
+++ fresh-coffee/react-fresh-coffee/src/context/QuioscoProvider.jsx
@@ -3,14 +3,13 @@
 import { createContext, useState, useEffect } from 'react'
 import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-
-import { categorias as categoriasDB } from '../data/categorias'
+import axios from 'axios'
 
 const QuioscoContext = createContext();
 
 const QuioscoProvider = ({children}) => {
-  const [categorias, setCategorias] = useState(categoriasDB);
-  const [categoriaActual, setCategoriaActual] = useState(categorias[0]);
+  const [categorias, setCategorias] = useState([]);
+  const [categoriaActual, setCategoriaActual] = useState({});
   const [modal, setModal] = useState(false);
   const [producto, setProducto] = useState({});
   const [pedido, setPedido] = useState([]);
@@ -21,6 +20,20 @@ const QuioscoProvider = ({children}) => {
       pedido.reduce( (total, producto) => (producto.precio * producto.cantidad) + total, 0 );
     setTotal(nuevoTotal);
   }, [pedido])
+
+  const obtenerCategorias = async () => {
+    try {
+      const { data } = await axios(`${import.meta.env.VITE_API_URL}/api/categorias`);
+      setCategorias(data.data);
+      setCategoriaActual(data.data[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect( () => {
+    obtenerCategorias();
+  }, [])
 
   const handleClickCategoria = id => {
     const categoria = categorias.filter(categoria => categoria.id === id)[0];
